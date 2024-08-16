f05c3a1a4dcd81dd98031f03bd430c739aa125cd
Sofia <sofiarrama55@gmail.com>
2024-08-16 17:33:02 -0300

    agregue interaccion al div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("contenedor").addEventListener("click", 
+        function() {
+            console.log("llego hasta aca");
+            alert("Hola, soy el div");
+        
+        });
+    });
